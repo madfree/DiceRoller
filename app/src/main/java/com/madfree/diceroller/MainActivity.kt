a63ee0955f1eb7fd7df6278bd19a11b0f41ef795
madfree <matzefrei@gmail.com>
2020-08-10 10:48:39 +0200

    feat: Added images for dices and code to display dice according to result of a roll

--- app/src/main/java/com/madfree/diceroller/MainActivity.kt
+++ app/src/main/java/com/madfree/diceroller/MainActivity.kt
@@ -3,8 +3,7 @@ package com.madfree.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -13,15 +12,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton : Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's roll"
         rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val resultText : TextView = findViewById(R.id.result_text)
         val randomInt = Random.nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
