961188cdb13e259227ff8043d053b406b58ef42f
madfree <matzefrei@gmail.com>
2020-08-10 13:09:56 +0200

    Changed initialization for dice image and added backward compatibility for vector drawables

--- app/src/main/java/com/madfree/diceroller/MainActivity.kt
+++ app/src/main/java/com/madfree/diceroller/MainActivity.kt
@@ -7,6 +7,9 @@ import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -27,7 +32,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage : ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
